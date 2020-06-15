50a53c0ef67e21ee282e569fa32f570d55a694f1
codewolf <726621241@qq.com>
2020-06-15 22:23:47 +0800

    算是一个小结束

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import _ from 'lodash';
+import './style.css';
+import girls from './girls.jpg';
+import Data from './data.xml';
 function component(){
     var element = document.createElement('div');
     element.innerHTML=_.join(['hello','webpack'],'');
+    element.classList.add('hello');
+    var mygirl=new Image();
+    mygirl.src=girls;
+    element.appendChild(mygirl);
+    console.log(Data);
     return element;
 }
 document.body.appendChild(component());
